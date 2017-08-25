7a294216d7463991b9a7322eab82e07125558a76
Derek <dezzy001@gmail.com>
2017-08-26 01:08:22 +1000

    first commit

---
+++ Assets/DiamondSquareTerrain.cs
@@ -0,0 +1,210 @@
+﻿/*
+Project 1
+Derek Chen, 766509
+Kevin Liu, 
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*sources: Unity Tutorials: 
+ 1) Diamond-Square Procedural Terrain - https://www.youtube.com/watch?v=1HV8GbFnCik
+(NOTE: tightly followed this tutorial for understanding of the diamond square algorithm)
+
+2) Diamond-square algorithm - https://en.wikipedia.org/wiki/Diamond-square_algorithm
+*/
+
+public class DiamondSquareTerrain : MonoBehaviour {
+	
+
+	//number of divisions (number of faces for the length or/hence the width) 
+	public int numDivisions; //(NOTE: always has to be a power of 2 )
+	//size of the terrain
+	public float size;
+
+
+	public float maxHeight; //the maximum height of the terrain
+	private float height; //current height of the terrain
+
+	//holds all the vertecies in an array
+	private Vector3[] verts;
+	//the count of verticies in the array
+	private int numVerts;
+
+	//mesh collider for the terrain
+	MeshCollider meshCollider;
+
+	// Use this for initialization
+	void Start () {
+
+		//terrain height is initially equal to max height 
+		height = maxHeight;
+
+		//get the mesh collider from the game object, which is later applied to the terrains mesh 	
+		meshCollider = GetComponent<MeshCollider>();
+
+		createTerrain ();
+
+	}
+
+
+	//terrain is created and initialised in this method
+	void createTerrain(){
+
+
+		/*Step 1: initialse terrain base, i.e a 2D square array of width and height (2^n) + 1 - i.e number of faces (divisons) + 1 */
+
+		numVerts = (numDivisions+1)*(numDivisions+1); //initialise total number of divisions
+		//initilaise the verticies, using just an 1d array to represent a 2d square (as we know the square parameters)
+		verts = new Vector3[numVerts];
+		//number of uv coordinates is equal to number of vertices
+		Vector2[] uvs = new Vector2[numVerts];
+
+		//number verticies for triangles = (number of divisions ^ 2) * 2 (two triangles in a square) * 3 (each triangle is defined by 3 verticies)
+		int[] triangles = new int[numDivisions*numDivisions*2*3];
+
+		//variables needed to generate terrain 
+		float halfSize = size * 0.5f;
+		float divisionSize = size / numDivisions;
+		int triOffset = 0; // keep track of where we are in the loop when building the triangles
+
+		//initialise a new mesh for the terrain 
+		Mesh mesh = new Mesh ();
+		GetComponent<MeshFilter> ().mesh = mesh;
+
+		/*Loop to insert the verticies (triangles) for the terrain base, step through the verticies row by row.
+		i represents the columns and j represents the rows*/
+		for(int i = 0; i<= numDivisions ; i++){
+
+			for(int j = 0; j <= numDivisions ;j++){
+
+				//step through verticies row by row, represented in a 1d array. Set up the square base of the entire terrain (i.e when height = 0)
+				verts[i*(numDivisions+1)+j] = new Vector3(-halfSize+j*divisionSize, 0.0f, halfSize-i*divisionSize);
+				//spreads out one texture to the whole terrain
+				uvs[i*(numDivisions+1)+j] = new Vector2((float)i/numDivisions,(float)j/numDivisions);
+
+				//only need to make traingles when we are less than numDivisions, for the rows and columns
+				if(i < numDivisions && j < numDivisions){
+
+					//coordinates of each triangles relative to each square face we are working with
+					int topLeft = i * (numDivisions+1) + j; 
+					int botLeft = (i+1) * (numDivisions+1) + j;
+					int topRight = topLeft + 1;
+					int botRight = botLeft + 1;
+
+					//define the vertices for triangles in clockwise fashion
+					//definition for the top right triangle in the square
+					triangles[triOffset] = topLeft;
+					triangles[triOffset+1] = topRight;
+					triangles[triOffset+2] = botRight;
+
+
+					//definition for the bottom left triangle in the square
+					triangles[triOffset+3] = topLeft;
+					triangles[triOffset+4] = botRight;
+					triangles[triOffset+5] = botLeft;
+
+					triOffset+=6;
+				}
+
+			}
+		}
+
+
+		/*step 2: set initial values to the four corner points of the square in the array*/
+
+		verts[0].y = Random.Range(-height,height); //top left corner
+		verts[numDivisions].y = Random.Range(-height,height);//top right corner
+		verts[verts.Length-1-numDivisions].y = Random.Range(-height,height);//bot left corner
+		verts[verts.Length-1].y = Random.Range(-height,height);//bot right corner
+
+
+		/*step 3: diamond and square steps*/
+
+		/*the number of times (iterations) which you have to perform a diamond-square algorithms 
+		(note: log^2(num of divisons) gives the number of times you have to perform, since there are 2^n faces)
+		*/
+		int iterations = (int)Mathf.Log(numDivisions,2);
+		int numSquares = 1; //variable that holds number of squares for each iteration
+		int squareSize = numDivisions;
+
+		//loop i is for the current iteration you are on
+		for(int i = 0; i < iterations;i++){
+
+			int row = 0;
+			//loop j and k are for the square steps
+			for(int j =0; j < numSquares; j++){
+
+				int col = 0;
+				for(int k = 0; k < numSquares; k++){
+
+					//do the diamond square algorithm to each square
+					diamondSquare(row, col, squareSize, height);
+					// jump through each column in a row by a square size
+					col += squareSize;
+				}
+
+				//go down a row by a squareSize
+				row += squareSize;
+
+			}
+
+			//the number of squares doubles each iteration, while the size of the square halves (e.g 4x4 --(square step)--> 2x2)
+			numSquares *= 2;
+			squareSize /= 2;
+			height *= 0.5f;// can edit this to lower the height after each iteration, or make it random ######CHANGE LATER########################
+
+		}
+
+
+
+		//assign all the variables to the mesh to be rendered
+		mesh.vertices = verts;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+
+		// used to apply mesh collider to the terrain
+		meshCollider.sharedMesh = mesh;
+	}
+
+
+	//the actual diamond square algorithm implementation
+	void diamondSquare(int row, int col, int squareSize, float heightOffset){
+		
+		int halfSize = (int)(squareSize*0.5f);//half of the square size
+
+		int topLeft = row * (numDivisions + 1) + col; //top left of a square
+		int botLeft = (row+squareSize) * (numDivisions+1) + col; //bottom left of a square (note: row+squareSize will give the bottom row)
+		int topRight = topLeft+squareSize;//top right of a square
+		int botRight = botLeft+squareSize;//bottom right of a square
+
+
+
+		//find the mid point of the square by obtaining half of the row size and add it onto half of the column size
+		int midPoint = (int)(row + halfSize) * (numDivisions + 1) +(int)(col+halfSize); 
+
+		//step 1: diamond step
+		//assign the midpoint vertex to the average of the corner verticies plus a random value (to give realism to the terrain)
+		verts [midPoint].y = (verts [topLeft].y + verts [topRight].y + verts [botLeft].y + verts [botRight].y) * 0.25f + Random.Range(-heightOffset,heightOffset);
+
+		//step 2: square step
+		//assign each diamond corners by averaging the previous 2 ajacent square corners and the mid point, then plus a random value to it
+		verts[topLeft+halfSize].y = (verts[topLeft].y + verts[topRight].y + verts[midPoint].y)/3 + Random.Range(-heightOffset,heightOffset) ; //top corner point
+		verts[midPoint-halfSize].y = (verts[topLeft].y + verts[botLeft].y + verts[midPoint].y)/3 + Random.Range(-heightOffset,heightOffset) ; //left corner point
+		verts[midPoint+halfSize].y = (verts[topRight].y + verts[botRight].y + verts[midPoint].y)/3 + Random.Range(-heightOffset,heightOffset) ;//right corner point
+		verts[botLeft+halfSize].y = (verts[botLeft].y + verts[botRight].y + verts[midPoint].y)/3 + Random.Range(-heightOffset,heightOffset) ;//bottom corner point
+
+
+	}
+
+
+
+
+
+}
+
---
+++ Assets/flightSim.cs
@@ -0,0 +1,146 @@
+﻿/*
+Project 1
+Derek Chen, 766509
+Kevin Liu, 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//sources: utilised sections of code to help with the mouse movements - https://forum.unity3d.com/threads/fly-cam-simple-cam-script.67042/
+
+
+
+public class flightSim : MonoBehaviour {
+
+	//variable that references to the terrain public parameters
+	private GameObject terrainObject;
+
+	public float speed; // speed of the camera 
+	public float camSensitivity; //How sensitive the camera is with mouse movements
+
+	private Vector3 prevMousePos; 
+
+	private int rollPos = 0; //position of the camera's current roll
+	public int rollSpeed; 
+
+
+	private int cameraOFFSET; // camera offset from the maximum height of the terrain
+
+	private float boundarySize; // the boundary which camera resides in (based on the terrain)
+
+
+
+
+	public Rigidbody rb;
+
+	void Start(){
+		
+
+		//get the terrain component and parameters
+		GameObject terrainObject = GameObject.Find("terrain"); //see if the terrain object exists
+		DiamondSquareTerrain terrain = terrainObject.GetComponent<DiamondSquareTerrain>(); // get the script 
+
+		boundarySize = terrain.size/2; //initialise boundarySize to half of the terrain size (since terrain is centered at origin)
+
+		//cameras rigid body
+		rb = GetComponent<Rigidbody>();
+
+		cameraOFFSET = (int)(terrain.maxHeight / 3);
+		//Initial camera view - position and rotation
+		transform.position = new Vector3 (0.0f, terrain.maxHeight+cameraOFFSET, 0.0f);
+		transform.eulerAngles = new Vector3(90, 0, 0);
+
+
+		prevMousePos = new Vector3(255, 255, 255);//centers the  mouse position, rather than the play button
+	}
+
+	void Update () {
+
+		//NOTE: use Time.deltaTime to make sure all movements have consistent frames per second
+
+		// prevent any external forces to affect the rigid body
+		rb.angularVelocity = Vector3.zero; //prevents external forces i.e collision to cause rotation
+		rb.velocity = Vector3.zero; //prevents external forces i.e collision to cause translation
+
+		/*mouse transformation - yaw and pitch*/
+		//previous mouse coordinate = current mouse position - the previous mouse position
+		prevMousePos = Input.mousePosition - prevMousePos ;
+		prevMousePos = new Vector3(-prevMousePos.y * camSensitivity, prevMousePos.x * camSensitivity, 0 );
+		prevMousePos = new Vector3(transform.eulerAngles.x + prevMousePos.x , transform.eulerAngles.y + prevMousePos.y, 0);
+
+		transform.eulerAngles = prevMousePos;
+		prevMousePos =  Input.mousePosition;
+
+
+		/*keyboard transformation- roll and camera translation*/
+
+		//camera translation (cannot go out of bounds)
+		Vector3 keyboardDirection = getTranslateKeyInput(); //get the keyboard inputs
+		
+		keyboardDirection = keyboardDirection * Time.deltaTime *speed;
+
+		//only allow translation if camera is within boundary size of the terrain
+		transform.position = new Vector3(
+			Mathf.Clamp (transform.position.x, -boundarySize, boundarySize),
+			transform.position.y,
+			Mathf.Clamp (transform.position.z, -boundarySize, boundarySize));
+
+		//move (translate) the camera by the keyboard direction
+		transform.Translate(keyboardDirection);
+
+
+		//roll, rotate around x axis according to key press
+		rollPos = getRotateKeyInput(rollPos);
+		transform.Rotate(Vector3.forward, rollPos);
+
+
+			
+	}
+
+
+
+
+	//returns the rotation position based on key press, adjusted for constant fps
+	private int getRotateKeyInput(int rollPos) { 
+		//Roll
+		if(Input.GetKey(KeyCode.Q))
+		{
+			//left roll
+			rollPos += (int)(rollSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.E))
+		{
+			//right roll
+			rollPos -= (int)(rollSpeed * Time.deltaTime);
+		}
+		return rollPos;
+	}
+
+
+
+	//returns the unit vector for camera velocity values based on keypress, or just a (0,0,0) vector if no key presses
+	private Vector3 getTranslateKeyInput() { 
+
+
+		Vector3 velocity = new Vector3(0.0f , 0.0f, 0.0f);
+
+		if (Input.GetKey (KeyCode.W)){ //move up
+			velocity += new Vector3(0.0f, 0.0f , 1.0f);
+		}
+		if (Input.GetKey (KeyCode.S)){ //move down
+			velocity += new Vector3(0.0f, 0.0f , -1.0f);
+		}
+		if (Input.GetKey (KeyCode.A)){ //move left
+			velocity += new Vector3(-1.0f, 0.0f , 0.0f);
+		}
+		if (Input.GetKey (KeyCode.D)){ //move right
+			velocity += new Vector3(1.0f,  0.0f , 0.0f);
+		}
+
+		return velocity;
+	}
+
+}
+
